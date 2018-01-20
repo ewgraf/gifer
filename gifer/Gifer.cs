c598bd1d780c07a627869da1be75165c86e8a13a
Ewgraf <ewgraf@users.noreply.github.com>
2018-01-20 22:57:39 +0300

    version update update

--- gifer/Gifer.cs
+++ gifer/Gifer.cs
@@ -4,7 +4,7 @@ using gifer.Utils;
 
 namespace giferWpf {
     public class Gifer {
-        public const string MyVersion = "0cc0d445-8ccf-4a97-bd30-84381e60a0ee";
+        public const string MyVersion = "4f9a48e1-e8b8-464b-8e6b-d756fe817ba3";
         public static readonly string[] KnownImageFormats =
             new[] { ImageFormat.Bmp, ImageFormat.Gif, ImageFormat.Jpeg, ImageFormat.Png, ImageFormat.Tiff }
             .Select(ImageExtensions.GetFilenameExtension)
