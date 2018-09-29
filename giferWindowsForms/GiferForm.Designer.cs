56ca509f7cb57091f9d3eff4546723abfcc2477d
Ewgraf <ewgraf@users.noreply.github.com>
2018-09-30 00:22:38 +0300

    stick keys to right

--- giferWindowsForms/GiferForm.Designer.cs
+++ giferWindowsForms/GiferForm.Designer.cs
@@ -52,6 +52,7 @@
             this.labelZoomIcon = new System.Windows.Forms.Label();
             this.labelCloseIcon = new System.Windows.Forms.Label();
             this.labelDragAndDrop = new System.Windows.Forms.Label();
+            this.labelHotkeys = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -80,12 +81,14 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.labelHotkeys);
+            this.groupBox1.Controls.Add(this.labelZoomKey);
+            this.groupBox1.Controls.Add(this.labelMoveKey);
             this.groupBox1.Controls.Add(this.labelMoveText);
             this.groupBox1.Controls.Add(this.labelMoveIcon);
             this.groupBox1.Controls.Add(this.labelDeleteIcon);
             this.groupBox1.Controls.Add(this.labelCloseKey1);
             this.groupBox1.Controls.Add(this.labelCloseKey0);
-            this.groupBox1.Controls.Add(this.labelZoomKey);
             this.groupBox1.Controls.Add(this.labelHelpText);
             this.groupBox1.Controls.Add(this.labelHelpKey);
             this.groupBox1.Controls.Add(this.labelPrevNextText);
@@ -94,17 +97,15 @@
             this.groupBox1.Controls.Add(this.labelCloseText);
             this.groupBox1.Controls.Add(this.labelDeleteKey);
             this.groupBox1.Controls.Add(this.labelPrevNextKey);
-            this.groupBox1.Controls.Add(this.labelMoveKey);
             this.groupBox1.Controls.Add(this.labelZoomIcon);
             this.groupBox1.Controls.Add(this.labelCloseIcon);
             this.groupBox1.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.groupBox1.ForeColor = System.Drawing.Color.DarkGray;
+            this.groupBox1.ForeColor = System.Drawing.Color.Gray;
             this.groupBox1.Location = new System.Drawing.Point(22, 8);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(406, 402);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Hotkeys";
             this.groupBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.groupBox1_DragDrop);
             // 
             // labelMoveText
@@ -112,7 +113,7 @@
             this.labelMoveText.AutoSize = true;
             this.labelMoveText.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelMoveText.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelMoveText.Location = new System.Drawing.Point(205, 77);
+            this.labelMoveText.Location = new System.Drawing.Point(179, 114);
             this.labelMoveText.Name = "labelMoveText";
             this.labelMoveText.Size = new System.Drawing.Size(50, 22);
             this.labelMoveText.TabIndex = 18;
@@ -123,7 +124,7 @@
             this.labelMoveIcon.AutoSize = true;
             this.labelMoveIcon.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelMoveIcon.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelMoveIcon.Location = new System.Drawing.Point(245, 66);
+            this.labelMoveIcon.Location = new System.Drawing.Point(219, 103);
             this.labelMoveIcon.Name = "labelMoveIcon";
             this.labelMoveIcon.Size = new System.Drawing.Size(45, 41);
             this.labelMoveIcon.TabIndex = 31;
@@ -134,7 +135,7 @@
             this.labelDeleteIcon.AutoSize = true;
             this.labelDeleteIcon.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelDeleteIcon.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelDeleteIcon.Location = new System.Drawing.Point(358, 295);
+            this.labelDeleteIcon.Location = new System.Drawing.Point(332, 274);
             this.labelDeleteIcon.Name = "labelDeleteIcon";
             this.labelDeleteIcon.Size = new System.Drawing.Size(39, 41);
             this.labelDeleteIcon.TabIndex = 23;
@@ -144,8 +145,8 @@
             // 
             this.labelCloseKey1.AutoSize = true;
             this.labelCloseKey1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelCloseKey1.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelCloseKey1.Location = new System.Drawing.Point(82, 177);
+            this.labelCloseKey1.ForeColor = System.Drawing.Color.Gray;
+            this.labelCloseKey1.Location = new System.Drawing.Point(90, 185);
             this.labelCloseKey1.Name = "labelCloseKey1";
             this.labelCloseKey1.Size = new System.Drawing.Size(80, 22);
             this.labelCloseKey1.TabIndex = 20;
@@ -155,8 +156,8 @@
             // 
             this.labelCloseKey0.AutoSize = true;
             this.labelCloseKey0.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelCloseKey0.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelCloseKey0.Location = new System.Drawing.Point(35, 162);
+            this.labelCloseKey0.ForeColor = System.Drawing.Color.Gray;
+            this.labelCloseKey0.Location = new System.Drawing.Point(43, 170);
             this.labelCloseKey0.Name = "labelCloseKey0";
             this.labelCloseKey0.Size = new System.Drawing.Size(63, 41);
             this.labelCloseKey0.TabIndex = 30;
@@ -166,8 +167,8 @@
             // 
             this.labelZoomKey.AutoSize = true;
             this.labelZoomKey.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelZoomKey.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelZoomKey.Location = new System.Drawing.Point(35, 110);
+            this.labelZoomKey.ForeColor = System.Drawing.Color.Gray;
+            this.labelZoomKey.Location = new System.Drawing.Point(106, 138);
             this.labelZoomKey.Name = "labelZoomKey";
             this.labelZoomKey.Size = new System.Drawing.Size(69, 41);
             this.labelZoomKey.TabIndex = 28;
@@ -178,7 +179,7 @@
             this.labelHelpText.AutoSize = true;
             this.labelHelpText.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelHelpText.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelHelpText.Location = new System.Drawing.Point(204, 264);
+            this.labelHelpText.Location = new System.Drawing.Point(178, 254);
             this.labelHelpText.Name = "labelHelpText";
             this.labelHelpText.Size = new System.Drawing.Size(120, 22);
             this.labelHelpText.TabIndex = 26;
@@ -188,8 +189,8 @@
             // 
             this.labelHelpKey.AutoSize = true;
             this.labelHelpKey.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelHelpKey.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelHelpKey.Location = new System.Drawing.Point(41, 264);
+            this.labelHelpKey.ForeColor = System.Drawing.Color.Gray;
+            this.labelHelpKey.Location = new System.Drawing.Point(130, 254);
             this.labelHelpKey.Name = "labelHelpKey";
             this.labelHelpKey.Size = new System.Drawing.Size(40, 22);
             this.labelHelpKey.TabIndex = 25;
@@ -200,18 +201,18 @@
             this.labelPrevNextText.AutoSize = true;
             this.labelPrevNextText.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelPrevNextText.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelPrevNextText.Location = new System.Drawing.Point(204, 220);
+            this.labelPrevNextText.Location = new System.Drawing.Point(178, 220);
             this.labelPrevNextText.Name = "labelPrevNextText";
-            this.labelPrevNextText.Size = new System.Drawing.Size(190, 22);
+            this.labelPrevNextText.Size = new System.Drawing.Size(200, 22);
             this.labelPrevNextText.TabIndex = 22;
-            this.labelPrevNextText.Text = "Prev←, next→ image";
+            this.labelPrevNextText.Text = "Prev← / next→ image";
             // 
             // labelDeleteText
             // 
             this.labelDeleteText.AutoSize = true;
             this.labelDeleteText.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelDeleteText.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelDeleteText.Location = new System.Drawing.Point(205, 307);
+            this.labelDeleteText.Location = new System.Drawing.Point(179, 286);
             this.labelDeleteText.Name = "labelDeleteText";
             this.labelDeleteText.Size = new System.Drawing.Size(160, 22);
             this.labelDeleteText.TabIndex = 24;
@@ -222,7 +223,7 @@
             this.labelZoomText.AutoSize = true;
             this.labelZoomText.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelZoomText.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelZoomText.Location = new System.Drawing.Point(204, 124);
+            this.labelZoomText.Location = new System.Drawing.Point(178, 150);
             this.labelZoomText.Name = "labelZoomText";
             this.labelZoomText.Size = new System.Drawing.Size(50, 22);
             this.labelZoomText.TabIndex = 21;
@@ -233,7 +234,7 @@
             this.labelCloseText.AutoSize = true;
             this.labelCloseText.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelCloseText.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelCloseText.Location = new System.Drawing.Point(205, 177);
+            this.labelCloseText.Location = new System.Drawing.Point(178, 185);
             this.labelCloseText.Name = "labelCloseText";
             this.labelCloseText.Size = new System.Drawing.Size(60, 22);
             this.labelCloseText.TabIndex = 15;
@@ -243,8 +244,8 @@
             // 
             this.labelDeleteKey.AutoSize = true;
             this.labelDeleteKey.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelDeleteKey.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelDeleteKey.Location = new System.Drawing.Point(41, 307);
+            this.labelDeleteKey.ForeColor = System.Drawing.Color.Gray;
+            this.labelDeleteKey.Location = new System.Drawing.Point(80, 286);
             this.labelDeleteKey.Name = "labelDeleteKey";
             this.labelDeleteKey.Size = new System.Drawing.Size(90, 22);
             this.labelDeleteKey.TabIndex = 7;
@@ -254,19 +255,19 @@
             // 
             this.labelPrevNextKey.AutoSize = true;
             this.labelPrevNextKey.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelPrevNextKey.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelPrevNextKey.Location = new System.Drawing.Point(41, 219);
+            this.labelPrevNextKey.ForeColor = System.Drawing.Color.Gray;
+            this.labelPrevNextKey.Location = new System.Drawing.Point(70, 219);
             this.labelPrevNextKey.Name = "labelPrevNextKey";
-            this.labelPrevNextKey.Size = new System.Drawing.Size(110, 22);
+            this.labelPrevNextKey.Size = new System.Drawing.Size(100, 22);
             this.labelPrevNextKey.TabIndex = 2;
-            this.labelPrevNextKey.Text = "[←] ,  [→]";
+            this.labelPrevNextKey.Text = "[←] / [→]";
             // 
             // labelMoveKey
             // 
             this.labelMoveKey.AutoSize = true;
             this.labelMoveKey.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelMoveKey.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelMoveKey.Location = new System.Drawing.Point(16, 62);
+            this.labelMoveKey.ForeColor = System.Drawing.Color.Gray;
+            this.labelMoveKey.Location = new System.Drawing.Point(87, 102);
             this.labelMoveKey.Name = "labelMoveKey";
             this.labelMoveKey.Size = new System.Drawing.Size(82, 41);
             this.labelMoveKey.TabIndex = 27;
@@ -277,7 +278,7 @@
             this.labelZoomIcon.AutoSize = true;
             this.labelZoomIcon.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelZoomIcon.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelZoomIcon.Location = new System.Drawing.Point(248, 114);
+            this.labelZoomIcon.Location = new System.Drawing.Point(222, 140);
             this.labelZoomIcon.Name = "labelZoomIcon";
             this.labelZoomIcon.Size = new System.Drawing.Size(37, 41);
             this.labelZoomIcon.TabIndex = 32;
@@ -288,7 +289,7 @@
             this.labelCloseIcon.AutoSize = true;
             this.labelCloseIcon.Font = new System.Drawing.Font("Consolas", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.labelCloseIcon.ForeColor = System.Drawing.Color.DarkGray;
-            this.labelCloseIcon.Location = new System.Drawing.Point(257, 170);
+            this.labelCloseIcon.Location = new System.Drawing.Point(230, 178);
             this.labelCloseIcon.Name = "labelCloseIcon";
             this.labelCloseIcon.Size = new System.Drawing.Size(36, 34);
             this.labelCloseIcon.TabIndex = 33;
@@ -304,6 +305,17 @@
             this.labelDragAndDrop.Size = new System.Drawing.Size(322, 24);
             this.labelDragAndDrop.TabIndex = 16;
             this.labelDragAndDrop.Text = "[Drag&&Drop GIF/Image Here]";
+            // 
+            // labelHotkeys
+            // 
+            this.labelHotkeys.AutoSize = true;
+            this.labelHotkeys.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHotkeys.ForeColor = System.Drawing.Color.Gray;
+            this.labelHotkeys.Location = new System.Drawing.Point(178, 79);
+            this.labelHotkeys.Name = "labelHotkeys";
+            this.labelHotkeys.Size = new System.Drawing.Size(80, 22);
+            this.labelHotkeys.TabIndex = 34;
+            this.labelHotkeys.Text = "Hotkeys";
             // 
             // GiferForm
             // 
@@ -331,27 +343,28 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-		private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timerUpdateTaskbarIcon;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label labelDragAndDrop;
-		private System.Windows.Forms.Label labelMoveKey;
-		private System.Windows.Forms.Label labelHelpText;
-		private System.Windows.Forms.Label labelHelpKey;
-		private System.Windows.Forms.Label labelPrevNextText;
-		private System.Windows.Forms.Label labelDeleteIcon;
-		private System.Windows.Forms.Label labelDeleteText;
-		private System.Windows.Forms.Label labelZoomText;
-		private System.Windows.Forms.Label labelCloseKey1;
-		private System.Windows.Forms.Label labelMoveText;
-		private System.Windows.Forms.Label labelCloseText;
-		private System.Windows.Forms.Label labelDeleteKey;
-		private System.Windows.Forms.Label labelPrevNextKey;
-		private System.Windows.Forms.Label labelZoomKey;
-		private System.Windows.Forms.Label labelCloseKey0;
+        private System.Windows.Forms.Label labelMoveKey;
+        private System.Windows.Forms.Label labelHelpText;
+        private System.Windows.Forms.Label labelHelpKey;
+        private System.Windows.Forms.Label labelPrevNextText;
+        private System.Windows.Forms.Label labelDeleteIcon;
+        private System.Windows.Forms.Label labelDeleteText;
+        private System.Windows.Forms.Label labelZoomText;
+        private System.Windows.Forms.Label labelCloseKey1;
+        private System.Windows.Forms.Label labelMoveText;
+        private System.Windows.Forms.Label labelCloseText;
+        private System.Windows.Forms.Label labelDeleteKey;
+        private System.Windows.Forms.Label labelPrevNextKey;
+        private System.Windows.Forms.Label labelZoomKey;
+        private System.Windows.Forms.Label labelCloseKey0;
         private System.Windows.Forms.Label labelMoveIcon;
         private System.Windows.Forms.Label labelZoomIcon;
         private System.Windows.Forms.Label labelCloseIcon;
+        private System.Windows.Forms.Label labelHotkeys;
     }
 }
 
